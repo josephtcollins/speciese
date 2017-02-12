e0c25ad0bd33666c2849ac81d650f2315214cf8b
Joe Collins <joe.collins121@gmail.com>
2017-02-12 14:41:35 -0700

    Init commit

---
+++ app/config/Root.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Router, Route, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
+
+import App from '../components/App';
+
+const history = useRouterHistory(createHashHistory)({ queryKey: false });
+
+export default class Root extends Component {
+  render() {
+    return (
+      <Router history={history} key={new Date()}>
+        <Route path="/" component={App} />
+      </Router>
+    );
+  }
+}
